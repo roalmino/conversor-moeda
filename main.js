70957ff9032485c7aa29e643bb35c3e269c70671
Rodrigo Almino <roalmino@outlook.com>
2022-03-12 13:46:18 -0300

    Create bitcoin conversion

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ function Converter() {
   var currency = document.querySelector(
     'input[name = "currency"]:checked'
   ).value
-
+  var valorBitcoin = (valorInserido / 198509.56).toFixed(6)
   if (currency == 'dolar') {
-    var valorConvertido = valorInserido * 5
-    answer.textContent = 'O valor convertido é de R$' + valorConvertido
+    var valorConvertido = (valorInserido / 5.08).toFixed(2)
+
+    answer.textContent = `O valor convertido é de US${valorConvertido} ou ${valorBitcoin} bitcoins`
   } else {
-    var valorConvertido = valorInserido * 10
-    answer.textContent = 'O valor convertido é de R$' + valorConvertido
+    var valorConvertido = (valorInserido / 5.54).toFixed(2)
+    answer.textContent = `O valor convertido é de €${valorConvertido} ou ${valorBitcoin} bitcoins`
   }
 }
